d29d8f9fe94de82ae26c4fa4b521dd4d81409f99
R.J <rjschodowski@hotmail.com>
2021-08-17 17:53:22 -0400

    JS code functionality added to project to be able to select people on the reviews page

--- app.js
+++ app.js
@@ -37,3 +37,60 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+//Select items
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+//Set starting item
+
+let currentItem = 0;
+
+//Load initial item
+
+window.addEventListener('DOMContentsLoaded', function() {
+  showPerson();
+})
+
+//Show person based on item
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//Show next person
+nextBtn.addEventListener('click', function() {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+//Show previous person
+
+prevBtn.addEventListener('click', function() {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+//Show random person
+
+randomBtn.addEventListener('click', function() {
+  currentItem = Math.floor(Math.random() * reviews.length)
+  showPerson();
+})
+
